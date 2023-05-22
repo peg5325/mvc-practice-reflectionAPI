9c4b257686b2e3817b75043d59d8cd7a923d54c5
Eongyu Park <peg5325@gmail.com>
2023-05-22 18:41:59 +0900

    @Service 애노테이션 생성 및 탐색

--- src/test/java/org/example/ReflectionTest.java
+++ src/test/java/org/example/ReflectionTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.annotation.Controller;
+import org.example.annotation.Service;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -23,6 +24,7 @@ public class ReflectionTest {
 
         Set<Class<?>> beans = new HashSet<>();
         beans.addAll(reflections.getTypesAnnotatedWith(Controller.class));
+        beans.addAll(reflections.getTypesAnnotatedWith(Service.class));
 
         logger.debug("beans : [{}]", beans);
     }
