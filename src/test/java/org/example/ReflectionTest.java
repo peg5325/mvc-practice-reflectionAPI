941d0549638842746ee509d08f641aa2e4e9c7a9
Eongyu Park <peg5325@gmail.com>
2023-05-22 19:18:29 +0900

    Reflection API -Class에 선언된 field, constructor, method 확인하기 -힙 영역에 로드돼 있는 클래스 타입의 객체를 가져오는 3가지 방법

--- src/test/java/org/example/ReflectionTest.java
+++ src/test/java/org/example/ReflectionTest.java
@@ -2,15 +2,20 @@ package org.example;
 
 import org.example.annotation.Controller;
 import org.example.annotation.Service;
+import org.example.model.User;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @Controller 애노테이션이 설정돼 있는 모든 클래스를 찾아서 출력한다.
@@ -25,6 +30,43 @@ public class ReflectionTest {
         Set<Class<?>> beans = getTypesAnnotatedWith(List.of(Controller.class, Service.class));
 
         logger.debug("beans : [{}]", beans);
+    }
+
+    @Test
+    void showClass() {
+        Class<User> clazz = User.class;
+        logger.debug(clazz.getName());
+
+        // getDeclaredFields() : Class에 선언된 모든 field를 출력하는 메소드
+        // getDeclaredConstructors() : Class에 선언된 모든 constructors(생성자)를 출력하는 메소드
+        // getDeclaredMethods() : Class에 선언된 모든 methods를 출력하는 메소드
+        logger.debug("User all declared fields: [{}]", Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList()));
+        logger.debug("User all declared constructors: [{}]", Arrays.stream(clazz.getDeclaredConstructors()).collect(Collectors.toList()));
+        logger.debug("User all declared methods: [{}]", Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList()));
+    }
+
+    /**
+     * 힙 영역에 로드돼 있는 클래스 타입의 객체를 가져오는 3가지 방법
+     */
+    @Test
+    void load() throws ClassNotFoundException {
+        // 1번
+        Class<User> clazz = User.class;
+
+        // 2번
+        User user = new User("eongyu", "박언규");
+        Class<? extends User> clazz2 = user.getClass();
+
+        // 3번
+        Class<?> clazz3 = Class.forName("org.example.model.User");
+
+        logger.debug("clazz: [{}]", clazz);
+        logger.debug("clazz2: [{}]", clazz2);
+        logger.debug("clazz3: [{}]", clazz3);
+
+        assertThat(clazz == clazz2).isTrue();
+        assertThat(clazz2 == clazz3).isTrue();
+        assertThat(clazz3 == clazz).isTrue();
     }
 
     private static Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
