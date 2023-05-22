3e297d04deb838e458cc5b54b0c6c41573fa1806
Eongyu Park <peg5325@gmail.com>
2023-05-22 18:39:23 +0900

    @Controller 애노테이션 생성 및 탐색

---
+++ src/test/java/org/example/ReflectionTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.example.annotation.Controller;
+import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Controller 애노테이션이 설정돼 있는 모든 클래스를 찾아서 출력한다.
+ */
+public class ReflectionTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+    @Test
+    void controllerScan() {
+        // org.example package 밑에 있는 class에 대해서 getTypesAnnotatedWith() 안에 있는 애노테이션이 붙은 class를 찾아서 해당 Hashset에 담는 코드
+        Reflections reflections = new Reflections("org.example");
+
+        Set<Class<?>> beans = new HashSet<>();
+        beans.addAll(reflections.getTypesAnnotatedWith(Controller.class));
+
+        logger.debug("beans : [{}]", beans);
+    }
+}
